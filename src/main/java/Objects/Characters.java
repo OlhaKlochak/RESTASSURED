5d5fe7b9f6e99a8581b8218d04e51b9aa9cbe336
Olha Klochak <olga.klochak@gmail.com>
2023-08-23 15:33:16 +0300

    Initial commit

---
+++ src/main/java/Objects/Characters.java
@@ -0,0 +1,11 @@
+package JsonObject;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class Characters{
+        public Info info;
+        public ArrayList<Result> results;
+    }
+
